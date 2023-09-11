4b0bb78872b832894c6493e0be22e6c4005cf082
Shri-0 <shrinath.narayanan@gmail.com>
2023-09-11 12:36:26 -0700

    "wave file generation"

--- wave_example.py
+++ wave_example.py
@@ -20,3 +20,23 @@ print("sample width", obj.getsampwidth())
 print("frame rate", obj.getnchannels())
 print("number of frames", obj.getnframes())
 print("parameters", obj.getparams())
+
+t_audio = obj.getnframes()/ obj.getframerate()
+
+print(t_audio)
+
+frames = obj.readframes(-1)  #will read all frames
+print(type(frames), type(frames[0]))
+print(len(frames)/2)
+
+obj.close()
+
+obj_new = wave.open("shri_new.wav", "wb")
+
+obj_new.setnchannels(1)
+obj_new.setsampwidth(2)
+obj_new.setframerate(16000.0)
+
+obj_new.writeframes(frames)
+
+obj_new.close()
