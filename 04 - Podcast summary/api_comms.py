c1b14642266a780403e66a86720eec47ca080d26
Shri-0 <shrinath.narayanan@gmail.com>
2023-09-21 19:34:24 -0700

    "api is working"

--- 04 - Podcast summary/api_comms.py
+++ 04 - Podcast summary/api_comms.py
@@ -4,6 +4,7 @@ from api_secrets import API_KEY_ASSEMBLYAI
 from api_secrets import API_KEY_LISTENNOTES
 import json
 import time
+import pprint
 
 
 headers = {'authorization': API_KEY_ASSEMBLYAI}
@@ -12,18 +13,20 @@ endpoint = "https://api.assemblyai.com/v2/transcript"
 listennotes_episode_endpoint = "https://listen-api.listennotes.com/api/v2/episodes"
 listennotes_headers = {'X-ListenAPI-Key': API_KEY_LISTENNOTES}
 
+
 def get_episode_audio_url(episode_id):
     url = listennotes_episode_endpoint + '/' + episode_id
     response = requests.request('GET', url, headers=listennotes_headers)
 
     data = response.json()
-    print(data)
+    pprint.pprint(data)
 
+    audio_url = data['audio']
+    episode_thumbnail = data['thumbnail']
+    podcast_title = data['podcast']['title']
+    episode_title = data['title']
 
-
-
-
-
+    return audio_url, episode_thumbnail, episode_title, podcast_title
 
 
 # 96d84944f2744f96ac33357653e5f68e
@@ -31,9 +34,11 @@ def get_episode_audio_url(episode_id):
 # start transcribing
 # audio_url = response.json()['upload_url']
 
-def transcribe(audio_url, sentiment_analysis):
-    transcript_request = {"audio_url": audio_url,
-                          "sentiment_analysis": sentiment_analysis}
+def transcribe(audio_url, auto_chapters):
+    transcript_request = {
+        "audio_url": audio_url,
+        "auto_chapters": auto_chapters
+    }
     transcript_response = requests.post(
         endpoint, json=transcript_request, headers=headers)
     return transcript_response.json()['id']
@@ -55,27 +60,34 @@ def poll(transc_id):
     return polling_response.json()
 
 
-def get_transcription_url(url, sentiment_analysis):
-    transcript_id = transcribe(url, sentiment_analysis)
+def get_transcription_url(url, auto_chapters):
+    transcript_id = transcribe(url, auto_chapters)
     while True:
         data = poll(transcript_id)
         if data['status'] == 'completed':
             return data, None
         elif data['status'] == 'error':
             return data, data['error']
-        print("Waiting 30 seconds")
-        time.sleep(30)
+
+        print("Waiting 60 seconds")
+        time.sleep(60)
 
 
-def save_transcript(url, title, sentiment_analysis=False):
-    data, error = get_transcription_url(url, sentiment_analysis)
+def save_transcript(episode_id):
+    audio_url, episode_thumbnail, episode_title, podcast_title = get_episode_audio_url(
+        episode_id)
+    data, error = get_transcription_url(audio_url, auto_chapters=True)
+
+    pprint.pprint(data)
+
+# 1:24:23 - This works so far
 
     if data:
-        text_filename = title + ".txt"
-        with open(text_filename, "w") as f:
-            f.write(data['text'])
+        #text_filename = title + ".txt"
+        #with open(text_filename, "w") as f:
+         #   f.write(data['text'])
 
-        if sentiment_analysis:
+        if auto_chapters:
             text_filename = title + "_sentiments.json"
             with open(text_filename, "w") as f:
                 sentiments = data["sentiment_analysis_results"]
